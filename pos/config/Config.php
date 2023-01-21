0d820cd56e4e583e8985b68bcde5d4896c13c0ef
vitooficial <m.elvin@live.com>
2023-01-20 21:19:16 -0600

    first commitssdddddcfd

--- pos/config/Config.php
+++ pos/config/Config.php
@@ -1,11 +1,11 @@
 <?php
-define('BASE_URL', 'https://pos.tri-facil.com/');
+define('BASE_URL', 'https://pos.tri-facil.com');
 define('HOST', 'localhost');
 define('USER', 'pos_invetory');
-define('PASS', 'invetory');
+define('PASS', '5KJ7Hu@sO1c@cG8P');
 define('DBNAME', 'pos_invetory');
 define('CHARSET', '');
-define('TITLE', 'POS VENTA');
+define('TITLE', 'invetory');
 define('MONEDA', 'S/ ');
 define('HOST_SMTP', 'smtp.gmail.com');
 define('USER_SMTP', 'tucorreo@gmail.com');
